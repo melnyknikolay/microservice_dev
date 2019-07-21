b91b617c5aa389ed9080b5574c58fdc06dfbc74a
Sergey <sergey.morenets@yandex.ua>
2019-07-21 10:43:53 +0300

    Task #6. Docker-compose configuration for notification service

--- notification/src/main/java/it/discovery/notification/NotificationApplication.java
+++ notification/src/main/java/it/discovery/notification/NotificationApplication.java
@@ -2,8 +2,10 @@ package it.discovery.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("it.discovery")
 public class NotificationApplication {
 
     public static void main(String[] args) {
